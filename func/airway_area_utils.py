a0e32f41d128c750609fb371f7571de395b222a0
sendoru <sendol39@gmail.com>
2023-07-30 14:19:21 +0900

    Update

--- func/airway_area_utils.py
+++ func/airway_area_utils.py
@@ -1,45 +1,79 @@
 from .detect_tree import *
 from collections import deque
+from skimage.measure import label
 
-def bfs(node, matrix, component_no: int):
-    # do BFS on 2d matrix
-    # unvisited node == -1, unreachable node == -2
-    dr = [1, -1, 0, 0]
-    dc = [0, 0, -1, 1]
+def bfs(node, matrix, unvisited_node_no=-1):
+    # do BFS on 3D matrix
+    # unvisited node == -1, unreachable node == very big value
+    ret = matrix.copy()
+    directions = [
+        (0, 0, -1),
+        (0, 0, 1),
+        (0, -1, 0),
+        (0, 1, 0),
+        (-1, 0, 0),
+        (1, 0, 0)
+    ]
+
     q = deque()
-    q.append(node)
+    q.append((*node, 0))
 
-    if matrix[node[0]][node[1]] != -1:
-        return 0
+    if ret[node[0]][node[1]][node[2]] != unvisited_node_no:
+        return ret
     
-    matrix[node[0]][node[1]] == component_no
-    area_size = 1
+    ret[node[0]][node[1]][node[2]] = 0
 
     while len(q) > 0:
-        r_cur, c_cur = q[-1]
+        x_cur, y_cur, z_cur, dist_cur = q[0]
         q.popleft()
-        for i in range(4):
-            r_next = r_cur + dr[i]
-            c_next = c_cur + dc[i]
-            if r_next < 0 or r_next >= matrix.shape[0] or c_next < 0 or c_next >= matrix.shape[1] or \
-                matrix[r_next][c_next] != -1:
-                continue
-            matrix[r_next][c_next] = component_no
-            area_size += 1
-            q.append([r_next, c_next])
-            
+        for i in range(len(directions)):
+            x_next = x_cur + directions[i][0]
+            y_next = y_cur + directions[i][1]
+            z_next = z_cur + directions[i][2]
+            if x_next < 0 or x_next >= ret.shape[0] or \
+                y_next < 0 or y_next >= ret.shape[1] or \
+                z_next < 0 or z_next >= ret.shape[2] or \
+                ret[x_next][y_next][z_next] != unvisited_node_no:
+                    continue
+            ret[x_next][y_next][z_next] = dist_cur + 1
+            q.append((x_next, y_next, z_next, dist_cur + 1))
 
-def get_voxel_by_generation(seg_result: np.array, connection_dict: dict):
-    ret = seg_result - 2
-    for key, val in connection_dict.items():
-        bfs([val['loc'][1], val['loc'][2]], ret[val['loc'][0]], val['generation'])
     return ret
 
-def get_voxel_count_by_generation(seg_result: np.array, connection_dict: dict):
-    voxel_by_generation = get_voxel_by_generation(seg_result, connection_dict)
+def is_upside_down(onehot:np.ndarray):
+    onehot_copied = onehot.copy()
+    comp_counts = []
+    for i in range(len(onehot_copied)):
+        _, comp_count = label(onehot_copied[i], return_num=True, connectivity=1)
+        if comp_count > 0:
+            comp_counts.append(comp_count * comp_count / onehot_copied[i].sum())
+
+    comp_counts = np.array(comp_counts)
+    forward = np.dot(comp_counts, np.linspace(1, len(comp_counts), len(comp_counts)))
+    backward = np.dot(comp_counts, np.linspace(len(comp_counts), 1, len(comp_counts)))
+    return backward < forward
+
+def get_voxel_by_generation(seg_result: np.ndarray, connection_dict: dict, max_valid_gen=15):
+    ret = np.zeros_like(seg_result) - 1
+    voxel_coords = np.argwhere(seg_result == 1)
+    min_dist = np.ones((len(voxel_coords), max_valid_gen + 1)) * 1e6
+    for key, val in connection_dict.items():
+        if val['generation'] <= max_valid_gen:
+            gen = val['generation']
+        else:
+            gen = max_valid_gen
+        min_dist[:,gen] = np.minimum(min_dist[:,gen], np.linalg.norm(np.array(val['loc'], dtype=float) - voxel_coords, axis=1))
+    gen_no = np.argmin(min_dist, axis=1)
+    for idx, voxel_coord in enumerate(voxel_coords):
+        ret[voxel_coord[0]][voxel_coord[1]][voxel_coord[2]] = gen_no[idx]
+    
+    return ret
+
+def get_voxel_count_by_generation(seg_result: np.array, connection_dict: dict, max_valid_gen=15):
+    voxel_by_generation = get_voxel_by_generation(seg_result, connection_dict, max_valid_gen)
     # generation higher than 16 is likely to be noise, ignore them
-    ret = np.zeros(16, int)
-    for i in range(16):
+    ret = np.zeros(max_valid_gen + 1, int)
+    for i in range(max_valid_gen + 1):
         ret[i] = (voxel_by_generation == i).astype(np.int32).sum()
     return ret
 
